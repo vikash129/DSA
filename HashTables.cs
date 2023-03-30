f6a7ae9acf65e3b4d2a78d79b626579885bebb43
vikash verma <74494285+vikash129@users.noreply.github.com>
2023-03-30 13:18:01 +0530

    d

--- HashTables.cs
+++ HashTables.cs
@@ -1,3 +1,41 @@
+using System;
+
+public class HashTable
+{
+   private static const int hashGroups = 10;
+   private list<pair<int, string>> table[hashGroups]; // list 1 , index 0 , list 2 ,index 1...
+
+    void Insert(int key, string value){
+
+ int hashKey = hashFunction(key);
+
+    // cout << "insert at ";
+    // cout << hashKey << endl;
+
+    auto &cell = table[hashKey];
+    auto bItr = begin(cell);
+
+    bool keyExists = false;
+
+    while (bItr != end(cell))
+    {
+        if (bItr->first == key)
+        {
+            keyExists = true;
+            cout << "[Warning] Key exits . value replaced from " << bItr->second << " -> " << value<<endl;
+            bItr->second == value;
+            break;
+        }
+        bItr++;
+    }
+
+    if (!keyExists)
+    {
+        cell.emplace_back(key, value);
+    }
+    }
+    
+    void Delete(int key){
    int hashKey = hashFunction(key);
 
     cout << "remove at ";
@@ -70,3 +108,79 @@ string HashTable ::search(int key){
     {
         cout << "[warning] key does not exist!!!!! \n";
     }
+    }
+
+    bool  Contains(int target){
+
+    }
+
+    int hashFunction(int key){
+    return key % hashGroups; // key:906, return 6
+
+    }
+
+
+    bool isEmpty()
+{
+    int sum = 0;
+
+    for (int i = 0 ; i < hashGroups; i++)
+    {
+        cout << table[i].size() << endl;;  
+        sum += table[i].size();
+    }
+
+    return !sum;
+};
+
+
+    void Print(){
+
+    };
+};
+
+
+
+
+
+
+
+int main()
+{
+
+    HashTable ht; 
+
+    if(ht.isEmpty()){
+        cout << "empty table\n";
+    }
+    else{
+        cout << "not empty table\n";
+    }
+
+
+ht.insert(231 , "vikash");
+ht.insert(241 , "tob");
+ht.insert(133 , "bob");
+ht.insert(248 , "sandy");
+ht.insert(836 , "lomy");
+ht.insert(130 , "nick");
+ht.insert(430 , "pick");
+ht.insert(130 , "dick");
+
+ht.printTable();
+
+
+// ht.remove(430 );
+// ht.remove(130 );
+// ht.remove(930 );
+
+// ht.printTable();
+
+ if(ht.isEmpty()){
+        cout << "empty table\n";
+    }
+    else{
+        cout << "not empty table\n";
+    }
+    return 0;
+}
